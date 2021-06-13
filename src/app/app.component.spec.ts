5d743a44e6bfec4067c188fe13f2e35af75060dd
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-06-13 13:15:19 +0200

    Change title app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Xampi');
+    expect(compiled.querySelector('h1').textContent).toContain('Padel');
   });
 });
