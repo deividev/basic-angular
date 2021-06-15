d072b5fbf33433e89d5dfb18aaf0af15532335e5
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-06-15 20:05:48 +0200

    Icon testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'padel-search';
+
+  faCoffee = faCoffee;
+  faUser = faUser;
 }
